dd19b00d5406bdb10879828f56c48d5760a890e2
fullcolor-alchemist <fullcolor-alchemist@hotmail.com>
2026-04-01 20:05:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ app.get("/", (req, res) => {
   res.send("Server läuft");
 });
 
-app.listen(3000, () => {
-  console.log("Server läuft auf Port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server läuft auf Port " + PORT);
 });
